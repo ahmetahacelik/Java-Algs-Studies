7fc299bf75d75d52031bbbb45eb20569fce3d3a3
ahmetahacelik <37809795+ahmetahacelik@users.noreply.github.com>
2019-02-15 16:52:08 +0300

    Update q10.java

--- q10.java
+++ q10.java
@@ -1,18 +1,12 @@
 import java.util.Arrays;
 
-// The transpose part was referred from: https://stackoverflow.com/questions/15449711/transpose-double-matrix-with-a-java-function/15497288
 
 public class q10 {
     
     public static void main(String[] args) {
         int matrix[][]={{2,1,5},{1,9,0},{1,5,9}};
         System.out.println(Arrays.deepToString(transpose(matrix)));
-    
-    
     }
-    
-    
-    
     
     private static int[][] transpose(int[][] matrix) {
         int[][] matrix2 = new int[matrix[0].length][matrix.length];
